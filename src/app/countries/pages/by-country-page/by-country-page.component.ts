74f874830116eac067c22f258625959364ed39ad
Lilia Borrazas Pristupa <lilia.borrazas@babelgroup.com>
2024-01-15 12:13:30 +0100

    page country

---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'countries-by-country-page',
+  templateUrl: './by-country-page.component.html',
+})
+export class ByCountryPageComponent {
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService){}
+
+  searchByPais(term:string): void{
+   this.countriesService.searchCountry(term)
+  .subscribe( countries =>{
+    this.countries = countries
+  });
+}
+}
